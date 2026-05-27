2e1c6c65963ce7b4144074bb1b58e99e269b66d0
Ng Wei Jien <ngweijien35@gmail.com>
2026-05-27 22:15:47 +0800

    add register route into backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def home():
     return "Hello CutSmart"
 
-@app.route("/api/test") 
+@app.route("/api/test")
 def test():
     return {"message": "Backend is working"}
+
+@app.route("/api/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    return {
+        "message": "Register route have received data",
+        "received": data
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
